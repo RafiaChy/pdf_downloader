f581905df44ff66e8c40044293295400935635e1
RafiaChySV <rafia.chowdhury@sensussoft.com>
2023-10-01 02:02:54 +0530

    code: loader removed

--- lib/features/02_Home/presentation/page/home_view.dart
+++ lib/features/02_Home/presentation/page/home_view.dart
@@ -5,7 +5,6 @@ import 'package:open_filex/open_filex.dart';
 import 'package:pdf_downloader/common/assets_manager.dart';
 import 'package:pdf_downloader/common/string_manager.dart';
 import 'package:pdf_downloader/common/widgets/background_image_holder.dart';
-import 'package:pdf_downloader/common/widgets/loading_widget.dart';
 import 'package:pdf_downloader/features/02_Home/presentation/bloc/home_bloc.dart';
 
 import '../widgets/url_field.dart';
@@ -34,69 +33,59 @@ class _HomeViewState extends State<HomeView> {
   },
   child: BlocBuilder<HomeBloc, HomeState>(
   builder: (context, state) {
-    if(state is HomeDownloading){
-      return LoadingWidget(child: buildHomeBody(height, state));
-    }
-    return buildHomeBody(height, state);
-  },
-),
-),
-);
-  }
-Scaffold buildHomeBody(double height, HomeState state) {
     return Scaffold(
     appBar: AppBar(
 
    backgroundColor: Colors.transparent,
-    elevation: 0,
-    automaticallyImplyLeading: false, //
-    centerTitle: true,
+      elevation: 0,
+      automaticallyImplyLeading: false, //
+      centerTitle: true,
 
-    title: Text(StringManager.appName, style: GoogleFonts.sail(fontSize: 24,color: Colors.brown,fontWeight: FontWeight.bold, )),
+      title: Text(StringManager.appName, style: GoogleFonts.sail(fontSize: 24,color: Colors.brown,fontWeight: FontWeight.bold, )),
 
-  ),
-    body: Form(
-      key: formKey,
-      child: Stack(
-        fit: StackFit.expand,
-        children: [
-          BackgroundImageHolder(height: height, imageAddress: AssetsManager.homeScreenBackground),
-          Container(
-            margin: EdgeInsets.all(height * 0.02),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                UrlField(textEditingController: textEditingController, height: height),
-                 SizedBox(height: height* 0.01,),
-                buildDownloadButton(height, state)
-              ],
-            ),
-          )
-        ],
-      ),
     ),
-  );
-  }
+      body: Form(
+        key: formKey,
+        child: Stack(
+          fit: StackFit.expand,
+          children: [
+            BackgroundImageHolder(height: height, imageAddress: AssetsManager.homeScreenBackground),
+            Container(
+              margin: EdgeInsets.all(height * 0.02),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  UrlField(textEditingController: textEditingController, height: height),
+                   SizedBox(height: height* 0.01,),
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: Colors.brown,
+                    ),
+                    padding: EdgeInsets.all(height * 0.01),
+                      child: IconButton(onPressed: (
 
-  Container buildDownloadButton(double height, HomeState state) {
-    return Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: Colors.brown,
-                  ),
-                  padding: EdgeInsets.all(height * 0.01),
-                    child: IconButton(onPressed: (
-
-                        ){
-                      if(formKey.currentState!.validate()) {
-                        _homeBloc.add(DownloadPdf(url: textEditingController.text));
-                      }
-                    }, icon:  Icon( state is HomeInitial? Icons.download :
-                    state is HomeDownloading ? Icons.downloading :
-                    state is HomeDownloaded? Icons.download_done :
-                    Icons.file_download_off,  color: Colors.white
-                       ),)
-                );
+                          ){
+                        if(formKey.currentState!.validate()) {
+                          _homeBloc.add(DownloadPdf(url: textEditingController.text));
+                        }
+                      }, icon:  Icon( state is HomeInitial? Icons.download :
+                      state is HomeDownloading ? Icons.downloading :
+                      state is HomeDownloaded? Icons.download_done :
+                      Icons.file_download_off,  color: Colors.white
+                         ),)
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  },
+),
+),
+);
   }
 }
 
