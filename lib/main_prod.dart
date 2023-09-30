e1167392e10fef85ed13cd2a03ce7603d212a6a5
RafiaChySV <rafia.chowdhury@sensussoft.com>
2023-09-30 18:50:11 +0530

    code: remove extra space

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -2,7 +2,6 @@ import 'package:pdf_downloader/app_environments/app_environments.dart';
 import 'package:pdf_downloader/main.dart';
 
 void main(){
-
   AppEnvironments.setUpEnvironment(Environments.prod);
   mainDelegateForEnvironments();
 }
